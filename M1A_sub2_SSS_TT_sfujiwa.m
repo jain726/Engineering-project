10ac30aab7ed47ddc10df6d5bbc9f43c43d3bfed
rrKenji <sfujiwa@purdue.edu>
2025-04-02 19:42:47 -0400

    Update M1A_sub2_SSS_TT_sfujiwa.m

--- M1A_sub2_SSS_TT_sfujiwa.m
+++ M1A_sub2_SSS_TT_sfujiwa.m
@@ -1,1 +1,5 @@
-function [out2] = M1A_sub#_SSS_TT_login(in2)
+function [out2] = M1A_sub2_SSS_TT_login(Test_data)
+% remove noise
+out2 = Test_data
+fprintf("Data Successfully passed to subfunction 2 programed by Saran")
+end
