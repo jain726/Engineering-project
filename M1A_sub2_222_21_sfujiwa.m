b9327a54bfae36607261852b1a50ea274a156bc0
rrKenji <sfujiwa@purdue.edu>
2025-04-02 19:46:42 -0400

    Update and rename M1A_sub2_SSS_TT_sfujiwa.m to M1A_sub2_222_21_sfujiwa.m

--- M1A_sub2_222_21_sfujiwa.m
+++ M1A_sub2_222_21_sfujiwa.m
@@ -1,4 +1,4 @@
-function [out2] = M1A_sub2_SSS_TT_login(Test_data)
+function [out2] = M1A_sub2_222_21_sfujiwa(Test_data)
 % remove noise
 out2 = Test_data
 fprintf("Data Successfully passed to subfunction 2 programed by Saran")
